bcf56eaf4f05005c94de41c856f14300404a0a6f
martinoprin <martinoprin8@gmail.com>
2025-01-11 15:04:02 +0100

    vaja 5 4

--- R4A/NSAv/Vaja5/index.php
+++ R4A/NSAv/Vaja5/index.php
@@ -48,6 +48,22 @@
 
         //$t = napolniBesede();
         //izpisiTabelo($t);
+        echo '<pre>';
+        $d= array(
+            "1431"=>array("ime"=>"Rok","natocenoGorivo"=>array(55,54,36,45,41)),
+            "1488"=>array("ime"=>"Vid","natocenoGorivo"=>array(70,72)),
+            "1492"=>array("ime"=>"Luka","natocenoGorivo"=>array(38,42,46,37,40,40))
+          );
+        $d[2231] = array("ime" => "Martin", "natocenoGorivo" => array(30));
+        $d[2231]["natocenoGorivo"][] = 41;
+
+        print_r($d);
+        foreach ($d as $key=>$value) {
+            if(count($value["natocenoGorivo"]) >= 3) {
+                echo "Sifra kupca=$key Ime=" . $value["ime"] . " Natočeno gorivo: " . implode(" ", $value["natocenoGorivo"]) . "<br>";
+            }
+        }
+          
     ?>
 </body>
 </html>
