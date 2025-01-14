75265173b84926292eb5b95fce971163f9e8f1a3
martinoprin <martinoprin8@gmail.com>
2025-01-14 19:41:43 +0100

    vaja12 - 3.naloga koncana

---
+++ R4A/NSAv/Vaja12/vozila.php
@@ -0,0 +1,7 @@
+<?php
+    $prostornina=array (
+        "Ford"=>array("Focus"=>1800,"Mondeo"=>1900),
+        "Mercedes"=>array("CLX"=>2800,"Meibach"=>3200,"razredB"=>1900),
+        "Kia"=>array("Ced"=>1400,"Sorento"=>2000,"Picanto"=>990,"Rio"=>1300)
+        );        
+?>
