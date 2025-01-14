00aacb9ab95684f34208b4b49d55b189624f3cfc
martinoprin <martinoprin8@gmail.com>
2025-01-14 18:22:14 +0100

    vaja 11 koncana

--- R4A/NSAv/vaja11/index.php
+++ R4A/NSAv/vaja11/index.php
@@ -7,66 +7,58 @@
 </head>
 <body>
 
+
+
+
+
     <form method="post" action="">
-        <label for="number">število:</label>
-        <input type="text" id="number" name="number">
+        <label for="text">Vnesite besedilo (največ 40 znakov):</label>
+        <input type="text" id="text" name="text" maxlength="40">
         <br>
-        <label for="type">Izberi sodost/lihost</label><br>
-        <input type="radio" id="odstrani_lihe" name="type" value="1" checked>
-        <label for="odstrani_lihe">Lihe</label>
-        <input type="radio" id="odstrani_sode" name="type" value="0">
-        <label for="odstrani_sode">Sode</label>
+        <label for="color">Izberite način izpisa:</label><br>
+        <input type="radio" id="color" name="display" value="color" checked>
+        <label for="color">Barvni</label>
+        <input type="radio" id="bw" name="display" value="bw">
+        <label for="bw">Črno/beli</label>
         <br>
-        <button type="submit">Briši števke</button>
+        <button type="submit">Izpiši besedilo</button>
     </form>
 
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST['number']) && is_numeric($_POST['number'])) {
-            $n = intval($_POST['number']);
-            $sl = intval($_POST['type']);
-            echo "nova vrednost: " . stevilo($n, $sl);
-        } else {
-            echo "Vpišite celo število.";
-        }
-    }
-    ?>
 
-    <br>
-    <a href="/c:/Users/marti/Desktop/martin/School/R4A/NSAv/vaja11/index.php">Nazaj</a>
-    <?php
-        function stevilo($n, $sl){
-            $poskus = 0;
-            $novaVrednost = 0;
-            while($n >= 1){
-                //echo $n . '<br>';
-                $ostanek = $n % 10;
-                //echo 'ostank: '. $ostanek . '<br>';
 
-                if($sl){
-                    if($ostanek%2==0){
-                        //echo 'sodo st';
-                        $novaVrednost += $ostanek * pow(10, $poskus);
-                        //echo '<br>nova vrednost: ' . $novaVrednost;
-                        $poskus++;
-                    }
+    <?php
+
+        
+        function barvniIzpis($text) {
+            $colors = ['red', 'green', 'blue'];
+            $colorIndex = 0;
+            $output = '';
+
+            for ($i = 0; $i < strlen($text); $i++) {
+                if ($text[$i] != ' ') {
+                    $output .= "<span style='color: " . $colors[$colorIndex] . ";'>" . htmlspecialchars($text[$i]) . "</span>";
+                    $colorIndex = ($colorIndex + 1) % 3;
+                } else {
+                    $output .= ' ';
                 }
-                else{
-                    if($ostanek%2!=0){
-                        //echo 'liho st';
-                        $novaVrednost += $ostanek * pow(10, $poskus);
-                        $poskus++;
-                    }
-                }
-                $n /= 10;
-                $n = floor($n);
-                
             }
-            return $novaVrednost;
+
+            return $output;
         }
-        $sl = 1; //ce je 0 odstrani sode ce je 1 odstrani lihe
-        $n = 2321;
-        //echo "nova vrednost: " . stevilo($n, $sl);
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (isset($_POST['text']) && strlen($_POST['text']) <= 40) {
+                $text = htmlspecialchars($_POST['text']);
+                $display = $_POST['display'];
+                if ($display == "color") {
+                    echo "<p>" . barvniIzpis($text) . "</p>";
+                } else {
+                    echo "<p style='color: black;'>$text</p>";
+                }
+            } else {
+                echo "Vnesite besedilo, ki je dolgo največ 40 znakov.";
+            }
+        }
     ?>
 </body>
 </html>
