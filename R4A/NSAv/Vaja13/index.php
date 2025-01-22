123268992d0cc5d42c4fe14ddf666e0f2a5ea966
Martin Oprin <martinoprin@Martins-MacBook-Air.local>
2025-01-22 07:59:30 +0100

    form

---
+++ R4A/NSAv/Vaja13/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vaja13</title>
+</head>
+<body>
+    <form action="vaja13.php" method="get">
+        <label for="stolpci">Vnesi število stolpcev:</label>
+        <input type="number" name="stolpci" pattern="^[0-9]$" required><br>
+        <label for="vrstce">Vnesi število vrstic:</label>
+        <input type="number" name="vrstce" pattern="^[0-9]$" required><br>
+        <label for="barva-diagonale">Vnesi barvo diagonale:</label>
+        <input type="color" name="barva-diagonale" required><br>
+        <label for="barva-ostale">Vnesi barvo ostalih celic:</label>
+        <input type="color" name="barva-ostale" required><br>
+        <input type="submit" value="Izrisi">
+
+    <?php
+        
+    ?>
+</body>
+</html>
